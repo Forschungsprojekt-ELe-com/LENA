7c3fbeacad850be336beacf9ffe7685bcd7e2b50
Jean-Luc Braun <braun@qualitus.de>
2023-06-21 09:23:54 +0200

    nur Ilias 7

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = "lena";
 $version = "0.0.1";
 $ilias_min_version = "7.0";
-$ilias_max_version = "8.999";
+$ilias_max_version = "7.999";
 $responsible = "Carsten Hellweg / Jean-Luc Braun";
 $responsible_mail = "hellweg@qualitus.de / braun@qualitus.de";
