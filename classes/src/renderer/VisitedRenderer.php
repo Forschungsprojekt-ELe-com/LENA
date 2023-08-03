293239527a6b5764859915d8349245b88f989e1f
Carsten Hellweg <hellweg@qualitus.de>
2023-08-03 14:11:35 +0200

    JL session

--- classes/src/renderer/VisitedRenderer.php
+++ classes/src/renderer/VisitedRenderer.php
@@ -15,7 +15,7 @@ class VisitedRenderer extends Renderer {
         for( $i=( count( $data ) - 1); $i >= 0 ; $i-- ) {
             $id = $data[ $i ];
             if( ! isset( $temp[ $id ] ) ) {
-                $temp[ $id ] = $id;
+                $temp[] = $id;
             }
         }
 //        $out .= '<pre>' . print_r( $temp, true ) . '</pre>';
