9de90ec66af409925b2e1d9dd579aae545ef3a10
Carsten Hellweg <hellweg@qualitus.de>
2023-08-11 10:46:38 +0200

    debugging DD

--- classes/src/emil/SuggestionFactoryMock.php
+++ classes/src/emil/SuggestionFactoryMock.php
@@ -5,7 +5,7 @@ class SuggestionFactoryMock extends SuggestionFactory {
     public function execute() {
         $filename = "ok";
         $temp = rand( 0, 100 );
-        if( $temp > 85 ) {
+        if( $temp > 101 ) {
             $filename = "nok";
         }
         $mockjson = file_get_contents( __DIR__ . '/mockdata/' . $filename . '.json' );
