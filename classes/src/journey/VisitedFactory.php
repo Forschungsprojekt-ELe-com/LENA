e47054fdd584d73ae5719c188842e8b02a05b751
carsten.hellweg <hellweg@qualitus.de>
2024-04-17 14:29:24 +0200

    historie bugfix

--- classes/src/journey/VisitedFactory.php
+++ classes/src/journey/VisitedFactory.php
@@ -19,10 +19,12 @@ class VisitedFactory {
         $sql = "
 SELECT ut_lp_marks.obj_id AS _obj_id
 FROM ut_lp_marks
-JOIN object_reference _or ON ( ut_lp_marks.obj_id=_or.obj_id )    
+JOIN object_reference _or ON ( ut_lp_marks.obj_id=_or.obj_id ) 
+JOIN object_data _od ON ( _or.obj_id=_or.obj_id)
 WHERE usr_id=" . $userId . "
   AND _or.deleted IS NULL
   AND status=2
+  AND _od.type = 'copa'
   AND ut_lp_marks.obj_id IN ( " . $inString . " )
 ORDER BY status_changed DESC
         ";
