f6517748c0fba0b21da58d993c5e9c2edf747d23
Jean-Luc Braun <braun@qualitus.de>
2024-05-03 10:03:17 +0200

    back to course

--- classes/src/renderer/BackToStartRenderer.php
+++ classes/src/renderer/BackToStartRenderer.php
@@ -5,7 +5,7 @@ class BackToStartRenderer extends Renderer {
     public function render() {
         $out = '';
                 
-        $out .= '<a href="goto.php?target=crs_' . $this->backToCourseId . '">back</a>';
+        $out .= '<a href="goto.php?target=crs_' . $this->backToCourseId . '" class="btn btn-default">Zur Übersicht</a>';
         
         return $out;
     }
