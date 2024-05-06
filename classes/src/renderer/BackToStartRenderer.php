c91d15b7900ca7b939cde3050b1950783795b769
Jean-Luc Braun <braun@qualitus.de>
2024-05-06 16:52:02 +0200

    UC4 Button

--- classes/src/renderer/BackToStartRenderer.php
+++ classes/src/renderer/BackToStartRenderer.php
@@ -4,8 +4,15 @@ class BackToStartRenderer extends Renderer {
     
     public function render() {
         $out = '';
-                
-        $out .= '<a href="goto.php?target=crs_' . $this->backToCourseId . '" class="btn btn-default">Zur Übersicht</a>';
+
+        $out .= '<a class="btn btn-bulky" id="back_to_course" ';
+        $out .= 'href="goto.php?target=crs_' . $this->backToCourseId . '">';
+        $out .= '<img class="icon small" alt="Zur Übersicht" ';
+        $out .= 'src="./Customizing/global/plugins/Services/COPage/PageComponent/LENA/templates/css/images/course.svg">';
+        $out .= '<span class="bulky-label">Zur Übersicht</span></a>';
+        
+        //$out .= '<a href="goto.php?target=crs_' . $this->backToCourseId . '" class="btn btn-default">Zur
+        // Übersicht</a>';
         
         return $out;
     }
